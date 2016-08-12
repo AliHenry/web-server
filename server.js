4316f800e3a0b8c56b7e230a30c4da195a257e16
Aliyu Henry <aliyuhgarba@gmail.com>
2016-08-13 02:13:06 +0700

    updated langauge

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(middleware.logger);
 //});
 
 app.get('/about', middleware.requireAuthentication, function(req, res) {
-   res.send('This is Aliyu about page'); 
+   res.send('This is Aliyu about page!'); 
 });
 
 app.use(express.static(__dirname + '/public'));
