d5aa312c7d771784ec22c8cade35eb42cf4678e4
Aliyu Henry <aliyuhgarba@gmail.com>
2016-08-13 02:10:21 +0700

    Initialize repo

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+var middleware = require('./middleware.js');
+
+app.use(middleware.logger);
+
+//app.use(middleware.requireAuthentication);
+
+//app.get('/', function(req, res) {
+//   res.send('Hello Aliyu Express'); 
+//});
+
+app.get('/about', middleware.requireAuthentication, function(req, res) {
+   res.send('This is Aliyu about page'); 
+});
+
+app.use(express.static(__dirname + '/public'));
+
+//console.log(__dirname);
+
+app.listen(PORT, function() {
+    console.log('Express server started on port', PORT);
+});
